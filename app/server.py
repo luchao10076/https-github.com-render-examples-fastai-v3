2a4ea89f9bc5e922048ed1c60dbb88b0aa6bf1f9
luchao10076 <39638297+luchao10076@users.noreply.github.com>
2020-05-07 22:23:29 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1YxL3H-EHdFpLbOA7u_THjg7sK9zsf2dl?alt=media&key=AIzaSyDa-vIGyKqkRTNaSLsoTmqw7nwDO9kP8mA'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1wEeerD3YHWyAdxH3qtpLfLVcjq4ZsOyh?alt=media&key=AIzaSyDa-vIGyKqkRTNaSLsoTmqw7nwDO9kP8mA'
 export_file_name = 'export.pth'
 
 classes = ['black', 'grizzly', 'teddys']
